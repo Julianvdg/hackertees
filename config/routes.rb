abbb76338462df3a2dfa7d20fa6f3429f33d760f
tmhage <attmhage@gmail.com>
2016-08-15 21:09:33 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  get 'shopping_carts/show'
+root "products#index"
+resources :products, :shopping_carts, :order_items
 
-  get 'order_lines/create'
-
-  get 'order_lines/update'
-
-  get 'order_lines/destroy'
-
-  get 'order_items/create'
-
-  get 'order_items/update'
-
-  get 'order_items/destroy'
-
-  get 'products/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
