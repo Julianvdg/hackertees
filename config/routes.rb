9a0e83a72197b727b26bcb37a3b248050bdc5c4a
ckreeftmeijer <ckreeftmeijer@gmail.com>
2016-08-16 13:53:25 +0200

    added shoppingcart in session

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 root "products#index"
 resources :products, :shopping_carts, :order_items
 
+get '/cart' => 'shopping_carts#index'
+get '/cart/clear' => 'shopping_carts#clearCart'
+get '/cart/:id' => 'shopping_carts#add'
+get '/cart/clearitem/:id' => 'shopping_carts#clearItem'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
