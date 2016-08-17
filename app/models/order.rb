3b123a883d27e8219224127f59cd6527adda6c85
tmhage <attmhage@gmail.com>
2016-08-17 17:54:59 +0200

    cleaned cart to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   has_many :order_lines
   has_many :products, :through => :order_lines
   belongs_to :user
+
+  def total_price
+    total = 0.0
+    self.order_lines.each do |orderline|
+      total += orderline[:sub_total].to_f
+    end
+    total
+  end
 end
