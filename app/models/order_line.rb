b0b3e1d410b26deea0a8d140f5d2285a5ee60b12
tmhage <attmhage@gmail.com>
2016-08-17 14:52:57 +0200

    add cart logic to orderline model

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,22 @@
 class OrderLine < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  before_save :before_save
+
+  def sub_total
+      unit_price * quantity
+  end
+
+  def unit_price
+    if persisted?
+      self.unit_price
+    else
+      product.price
+    end
+  end
+
+  def before_save
+    self.unit_price = unit_price
+    self.total_price = quantity * self.unit_price
+  end
 end
