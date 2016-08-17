420210fcd4664f5bcd9e1ee0ecc3f05d14d41ef0
tmhage <attmhage@gmail.com>
2016-08-17 15:47:47 +0200

    add user ref and relations to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_lines
   has_many :products, :through => :order_lines
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816122908) do
+ActiveRecord::Schema.define(version: 20160817134352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160816122908) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.boolean  "confirmed"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -76,5 +78,6 @@ ActiveRecord::Schema.define(version: 20160816122908) do
 
   add_foreign_key "order_lines", "orders"
   add_foreign_key "order_lines", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "user_profiles", "users"
 end
