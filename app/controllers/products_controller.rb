70aad5528fe84d4c0cead5200065527606f565d9
tmhage <attmhage@gmail.com>
2016-08-18 16:01:32 +0200

    created semi scaffold for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,43 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :product_image, :stock)
   end
 end
