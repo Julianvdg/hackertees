49d029cf5267ac0113d5c3e0d5c8f5c4f9cee052
reinoptland <reinoptland@gmail.com>
2016-08-21 12:27:17 +0200

    Add remote links to indexpage

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -16,7 +16,10 @@ class ShoppingCartsController < ApplicationController
         cart[id] = 1
       end
 
-      redirect_to root_path
+      respond_to do |format|
+        format.html {redirect_to root_path }
+        format.js
+      end
   end
 
   def clearCart
